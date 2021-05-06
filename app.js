b3650e91159a7744caa4490169aca2c8d75f63a2
eluka22 <eluka22@gmail.com>
2021-05-06 16:07:24 +0900

    Initial commit

---
+++ app.js
@@ -0,0 +1,30 @@
+require("dotenv").config();
+
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+
+const indexRouter = require("./routes/index");
+
+const app = express();
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/", indexRouter);
+
+app.use((req, res, next) => {
+  next(createError(404));
+});
+
+app.use((err, req, res, next) => {
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+  res.status(err.status || 500).send(err.message);
+});
+
+module.exports = app;
