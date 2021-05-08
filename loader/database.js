7c6d069af35b91e369d5c950ff8ce3e6fe4e0920
eluka22 <eluka22@gmail.com>
2021-05-08 15:34:02 +0900

    Add: database loader

---
+++ loader/database.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const connectMongoDB = () => {
+  mongoose.connect(process.env.MONGO_DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+    dbName: "game",
+  });
+
+  const db = mongoose.connection;
+  db.on("error", (err) => console.error(`DB connection Error : \n${err}`));
+  db.once("open", () => console.log("MongoDB Connected"));
+};
+
+module.exports = connectMongoDB;
